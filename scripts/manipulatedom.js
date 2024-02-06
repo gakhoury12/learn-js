915f057f8fbdef925bd3d8278328f61e0bc3d6ac
Gabe Khoury <gabrielkhoury3@gmail.com>
2024-02-06 15:31:28 -0500

    new branch

--- scripts/manipulatedom.js
+++ scripts/manipulatedom.js
@@ -13,6 +13,7 @@ function createTRNode(colNodes) {
 function createTDNode(childNode) {
   let tdNode = document.createElement("td");
   tdNode.classList.add('tdNode');
+	tdNode.classList.add('tdNode');
   tdNode.appendChild(childNode);
   return tdNode;
 }
@@ -25,8 +26,7 @@ function createTxtNode(txt) {
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
-    let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    document.getElementById("value");
   }
   document.getElementById("root").appendChild(tableNode);
 }
